a6f618f7675f6533898d2427d2d7f825fa6c1f4a
Endeavour <martin.wonders@unn.ac.uk>
2017-03-27 17:47:45 +0100

    added string concat function

--- Week1/Ex2_mw.c
+++ Week1/Ex2_mw.c
@@ -3,19 +3,29 @@
 
 int string_length(char aString[]);
 int string_compare(char string1[], char string2[]);
-char* string_concat(char string1[], char string2[]);
+char *string_concat(char string1[], char string2[]);
+char *string_replace(char aString[], char replaced, char replacement);
 
 int main(int argc, char *argv[]) {
-    printf("The string %s is %i characters long.\n", argv[1], string_length(argv[1]));
-	if ( argc == 3 ) {
-		if ( string_compare(argv[1],argv[2]) ) {
-			printf("The sring %s is the same as the string %s.\n", argv[1], argv[2]);
-		}
-		else {
-			printf("The string %s is not the same as %s.\n", argv[1], argv[2]);
-		}
-		printf("Joining the strings %s and %s together results in: %s.\n", argv[1],argv[2],string_concat(argv[1], argv[2]));
+    if ( argc == 2) {
+        printf("The string %s is %i characters long.\n", argv[1], string_length(argv[1]));
+    }
+    else if ( argc == 5 ) {
+	if ( string_compare(argv[1],"-r") ) {
+	    printf("The new string is: %s\n", string_replace(argv[2],argv[3][0],argv[4][0]));
 	}
+
+    }
+    else if ( argc == 3 ) {
+        if ( string_compare(argv[1],argv[2]) ) {
+	    printf("The sring %s is the same as the string %s.\n", argv[1], argv[2]);
+        }
+	else {
+	    printf("The string %s is not the same as %s.\n", argv[1], argv[2]);
+	}
+	printf("Joining the strings %s and %s together results in: %s.\n", argv[1],argv[2],string_concat(argv[1], argv[2]));
+    }
+
     return 0;
 }
 
@@ -61,4 +71,18 @@ char* string_concat(char string1[], char string2[]) {
 		new_string[i] = string2[i - s2_length];
 	}
 	return new_string;
+}
+
+char *string_replace(char aString[], char replaced, char replacement){
+    int length = string_length(aString);
+    char *new_string  = (char*) malloc(sizeof(char) * length + 1);
+    for (int i = 0; i < length; i++) {
+        if ( aString[i] == replaced ) {
+            new_string[i] = replacement;
+        }
+        else {
+            new_string[i] = aString[i];
+        }
+    }
+    return new_string;
 }
